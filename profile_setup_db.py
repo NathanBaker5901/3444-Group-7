84c09483f7c6440f823a66a87cb33c770a15b317
Montoya, Abel <abelmontoya@my.unt.edu>
2024-07-20 23:39:43 -0500

    Made comments for profile database and users database

--- profile_setup_db.py
+++ profile_setup_db.py
@@ -1,8 +1,20 @@
-import sqlite3
+import sqlite3 # This portions is for the importation of the sqlite3 module to work with the sqlite database
 
+# Function to initialize the profiles database
 def init_profile_db():
+    # Using context managaer to connect ti the SQL database with the name "profiles.db"
+    # This will automatically commit and close connection
     with sqlite3.connect('profiles.db') as conn:
+
+        # Creates a curser object for database interactions
         c = conn.cursor()
+
+        # Executes an SQL statement to make a table called "profiles" if there is not one
+        # Will contain 4 columns
+        # "id" : integer that funstions as primary key and increments automatically 
+        # "username" : This will house the user username and it cannot be null
+        # "bio" : This is a text box the houses the user entered bio
+        # "profile_pic" : Text field to store the URL path to the image *Work In Progress*
         c.execute('''
             CREATE TABLE IF NOT EXISTS profiles (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -11,7 +23,9 @@ def init_profile_db():
                 profile_pic TEXT
             )
         ''')
+        # Commit the changes to database
         conn.commit()
 
+# If this script is run as the main module then call the init_profile_db function to initialize the database
 if __name__ == '__main__':
     init_profile_db()
--- setup_db.py
+++ setup_db.py
@@ -1,12 +1,25 @@
-import sqlite3
+import sqlite3 # This portions is for the importation of the sqlite3 module to work with the sqlite database
 
-# Connect to the database (or create it if it doesn't exist)
+# Connect to the database "users.db" or create if it does not exist
 conn = sqlite3.connect('users.db')
+
+# Creates a curser object for database interactions
 c = conn.cursor()
 
-# Create the users table
+# Executes an SQL statement to make a table called "users" if there is not one
+# Will contain 4 columns
+# "id" : integer that funstions as primary key and increments automatically 
+# "username" : This will house the user username and it cannot be null
+# "email" : This will house the user email and cannot be null
+# "password" : This houses the user made password and cannot be null
 c.execute('''CREATE TABLE IF NOT EXISTS users
              (id INTEGER PRIMARY KEY, 
               username TEXT UNIQUE NOT NULL, 
               email TEXT UNIQUE NOT NULL, 
               password TEXT NOT NULL)''')
+
+#These are not necessary due to the script doing it autamatically but i read that it is best practice
+# Commits changes to the database
+conn.commit()
+# Close the connection to the database
+conn.close()
